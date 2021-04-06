4df163e89eeee0bec574d3b5dc56d1274b8af54e
ReactDev1337 <82044444+ReactDev1337@users.noreply.github.com>
2021-04-06 21:13:24 +0200

    added numbers.py

---
+++ python/numbers.py
@@ -0,0 +1,9 @@
+import sys
+
+first_number = float(sys.argv[1])
+second_number = float(sys.argv[2])
+
+result_sum = first_number + second_number
+result_difference = first_number - second_number
+result_product = first_number * second_number
+result_quotient = first_number / second_number
