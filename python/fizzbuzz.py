f2ece360ab3b1453ef79c003ea9d580d7955c26c
ReactDev1337 <82044444+ReactDev1337@users.noreply.github.com>
2021-04-06 21:10:27 +0200

    added fizzbuzz.py

---
+++ python/fizzbuzz.py
@@ -0,0 +1,15 @@
+import sys
+
+args = sys.argv
+args.pop(0)
+
+for i in args:
+    i = int(i)
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
